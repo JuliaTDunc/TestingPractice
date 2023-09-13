9dbec3e149a66b7632f5a629563345375715a124
Julia Duncan <juliataylorduncan@gmail.com>
2023-09-12 23:57:00 -0700

    Funcs.jsCompleted

--- funcs.js
+++ funcs.js
@@ -1,13 +1,31 @@
 function isFive(num) {
-  // Your code here
+  if(num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isOdd(number) {
-  // Your code here
+  if (number % 2 === 1 || number % 2 === -1) {
+    return true;
+  } 
+  if(number % 2 === 0) {
+    return false;
+  }
+  if(typeof number !== "number") {
+    throw new Error("argument is not a typeof 'number'");
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let resArr = [];
+  if(min < max) {
+    for(let i = min; i <= max; i += step) {
+      resArr.push(i);
+    }
+  }
+  return resArr;
 }
 
 
